e6f5798d5d6c3987bd4f1ff3ef4939a93fa289b1
Sam Phippen <samphippen@googlemail.com>
2014-02-27 16:08:01 +0000

    I made it more complicated I'm so sorry

--- app.py
+++ app.py
@@ -3,6 +3,10 @@ import os
 
 from flask import Flask
 from matches import ConfigReader
+from who import CurrentMatcher
+import yaml
+from urllib2 import urlopen
+
 app = Flask(__name__)
 
 @app.route('/n_matches')
@@ -10,12 +14,45 @@ def n_matches():
     return str(ConfigReader(open("config.yml").read()).n_matches())
 
 
-@app.route('/current_match')
+@app.route('/current_match_number')
 def current_match():
     return str(ConfigReader(open("config.yml").read()).current_match())
 
 
+def read_match_number(match_number):
+    if match_number != -1:
+        cm = CurrentMatcher(open("config.yml").read())
+        return yaml.dump(
+            {
+                "arena_0": cm.whos_in(match_number, "arena_0"),
+                "arena_1": cm.whos_in(match_number, "arena_1")
+            }
+        )
+    else:
+        return yaml.dump({})
+
+def current_match_number():
+    return int(ConfigReader(open("config.yml").read()).current_match())
+
+@app.route('/who/<match_number>')
+def who(match_number):
+    return read_match_number(int(match_number))
+
+@app.route("/current_match_teams")
+def current():
+    return read_match_number(current_match_number())
+
+
+@app.route('/next_match_teams')
+def next():
+    return read_match_number(current_match_number()+1)
+
+@app.route('/match_after_next_teams')
+def after_next():
+    return read_match_number(current_match_number()+2)
+
+
 if __name__ == '__main__':
     # Bind to PORT if defined, otherwise default to 5000.
-    port = int(os.environ.get('PORT', 5000))
+    port = int(os.environ.get('PORT', 5112))
     app.run(host='0.0.0.0', port=port)
--- readme.md
+++ readme.md
@@ -16,7 +16,7 @@ it's got **endpoints**
 
 the number of matches
 
-/current_match
+/current_match_number
 
 the current match number
 ```
@@ -28,3 +28,46 @@ config.yml has a current delay value, which is seconds. Set it based on how
 behind we are, watch get dealt with disappear.
 
 **CAN YOU BELIEVE IT'S THAT SIMPLE**
+
+#I'm sorry it got more complicated
+
+Now it knows which matches people are in too
+
+#SRobo who is in this match tool
+
+##Command line
+
+run `./who.py <match_number>`
+
+##Web
+
+run `pip install flask` then run `python app.py`
+
+it's got **endpoints**
+
+```
+/who/<match_number>
+
+shows who is in the match with match number (zero indexed)
+
+/current_match_teams
+
+shows who is in the current match
+
+/next_match_teams
+
+shows who is in the next match
+
+/match_after_next_teams
+
+shows who is in the match after the next match
+```
+
+All responses are YAML
+
+##CONFIGURING
+
+![Configuring is hard](http://4.bp.blogspot.com/-fYJrkNWec08/T9EYOmNGPNI/AAAAAAAAC04/UtdRRM8a3hc/s640/cat-fat-dancing-cat-gif.gif)
+
+Give it a config.yml that looks like the one in this repository but with
+more matches because there will be more matches
