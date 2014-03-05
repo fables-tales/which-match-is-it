4595d7fe2f4872cc563d07d08dca4bbd919847f8
Rob Spanton <rspanton@zepler.net>
2014-03-05 01:15:47 +0000

    Rework matches HTTP API
    There are now three endpoints:
    
    /matches/<arena>/<match_number>
    
      Gives you information about the given match number in the specified
      arena.
    
    /matches/<arena>/<match_min>-<match_max>
    
      Gives you information about the range of match numbers in the
      specified arena.
    
    /matches/<arena>/current
    
      Gives you information about the current match that is happening in
      the specified arena.

--- app.py
+++ app.py
@@ -1,60 +1,67 @@
 #!/usr/bin/env python
 import os
-from flask import Flask
-import json
-from srcomp.matches import MatchSchedule
+from flask import Flask, jsonify, json
+from srcomp import SRComp
 
-CONFIG_FNAME = os.path.join(os.path.dirname(__file__),
-                            "config.yml")
+ROOTDIR = os.path.dirname(__file__)
 
 app = Flask(__name__)
 
-def get_schedule():
-    return MatchSchedule(CONFIG_FNAME)
+def get_comp():
+    return SRComp(ROOTDIR)
 
-@app.route('/n_matches')
-def n_matches():
-    return str(get_schedule().n_matches())
+def match_json_info(match):
+    return {
+        "number": match.num,
+        "arena": match.arena,
+        "teams": match.teams,
+        "start_time": match.start_time.isoformat(),
+        "end_time": match.end_time.isoformat(),
+    }
 
+@app.route("/matches/<arena>/<int:match_number>")
+def match_info(arena, match_number):
+    "Get information about the given match number"
+    comp = get_comp()
 
-@app.route('/current_match_number')
-def current_match():
-    return str(get_schedule().current_match())
+    if match_number not in comp.schedule.matches:
+        return jsonify(error=True,msg="Invalid match number"), 404
+    match = comp.schedule.matches[match_number]
 
+    if arena not in match:
+        return jsonify(error=True,msg="Invalid arena"), 404
+    match = match[arena]
 
-def read_match_number(match_number):
-    if match_number != -1:
-        cm = get_schedule()
-        m = cm.matches[match_number]
+    return jsonify(**match_json_info(match))
 
-        r = {}
-        for match in m.values():
-            r[match.arena] = match.teams
+@app.route("/matches/<arena>/<int:match_number_min>-<int:match_number_max>")
+def match_info_range(arena, match_number_min, match_number_max):
+    "Get information about the given range of matches"
+    comp = get_comp()
+    resp = {}
 
-        return json.dumps(r)
-    else:
-        return json.dumps({})
+    for match_n in range(match_number_min, match_number_max+1):
+        if match_n not in comp.schedule.matches:
+            "Skip matches that don't exist"
+            continue
 
-def current_match_number():
-    return int(get_schedule().current_match())
+        resp[match_n] = match_json_info(comp.schedule.matches[match_n][arena])
 
-@app.route('/who/<match_number>')
-def who(match_number):
-    return read_match_number(int(match_number))
+    return json.dumps(resp)
 
-@app.route("/current_match_teams")
-def current():
-    return read_match_number(current_match_number())
+@app.route("/matches/<arena>/current")
+def current_match_info(arena):
+    comp = get_comp()
+    current = comp.schedule.current_match()
 
+    if current is None:
+        return jsonify(number=None, msg="No current match")
 
-@app.route('/next_match_teams')
-def next():
-    return read_match_number(current_match_number()+1)
+    if arena not in current:
+        return jsonify(error=True,msg="Invalid arena"), 404
+    match = current[arena]
 
-@app.route('/match_after_next_teams')
-def after_next():
-    return read_match_number(current_match_number()+2)
-
+    return jsonify(**match_json_info(match))
 
 if __name__ == '__main__':
     # Bind to PORT if defined, otherwise default to 5000.
